9dbfa944ab1b090b7d64fa16e246f9c3a26898f8
Ben Garcia <ben@seebenprogram.com>
2018-02-09 20:45:04 -0500

    Create separate file to stage development of components

--- assets/javascript/components.js
+++ assets/javascript/components.js
@@ -66,3 +66,24 @@ Component #8, 'START OVER' button
   <button id="js-reset-button" class="button large alert">Start Over?</button>
 </div>
 */
+
+// jQuery selector
+// $('#js-page-content').
+
+// Views that need to be assembled, rendered
+/*
+ * Game Begins:
+ *     - Component #1
+ * 
+ * Question Asked:
+ *     - Components #2, #3, #4
+ * 
+ * Question Answered wrong:
+ *     - Components #2, #3, #5
+ * 
+ * Question Answered right:
+ *     - Components #2, #3, #6
+ * 
+ * Game Over:
+ *     - Components #7, #8
+ */
