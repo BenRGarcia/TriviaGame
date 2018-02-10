f7ac2236e0c421f34dae64f0ec55c0ff70ddbb97
Ben Garcia <ben@seebenprogram.com>
2018-02-09 19:58:17 -0500

    Initial commit, add VIEW pseudocode

---
+++ assets/javascript/components.js
@@ -0,0 +1,68 @@
+// VIEW 
+// - Components Pseudocode
+
+/*
+Component #1, 'START' button
+<div id="js-component-start" class="cell">
+  <button id="js-start-button" class="button large alert">START</button>
+</div>
+*/
+
+/*
+Component #2, 'TIMER'
+<div id="js-component-timer" class="cell shrink content output">
+  <h3>Time Remaining: <span id="js-time-remaining">30</span></h3>
+</div> 
+*/
+
+/*
+Component #3, 'TRIVIA QUESTION'
+<div id="js-component-question" class="cell content">
+  <h2>In what month is the Earth closest to the sun?</h2>
+</div> 
+*/
+
+/*
+Component #4, 'TRIVIA ANSWERS'
+<div id="js-component-answers" class="cell content shrink output">
+  <h4 id="js-choice-1" class="choices">January</h4>
+  <h4 id="js-choice-2" class="choices">April</h4>
+  <h4 id="js-choice-3" class="choices">July</h4>
+  <h4 id="js-choice-4" class="choices">October</h4>
+</div>
+*/
+
+/*
+Component #5, 'INCORRECT RESPONSE'
+<div id="js-component-incorrect" class="cell content shrink output">
+  <h4>NOPE!</h4>
+  <h4>The correct answer was:</h4>
+  <h4>January</h4>
+  <img src="./assets/images/sad-earth.gif">
+</div>
+*/
+
+/*
+Component #6, 'CORRECT RESPONSE'
+<div id="js-component-correct" class="cell content shrink output">
+  <h4>Correct!</h4>
+  <img src="./assets/images/dancing-earth.gif">
+</div> 
+*/
+
+/*
+Component #7, 'FINAL STATS'
+<div id="js-component-reset" class="cell content shrink output">
+  <h2>All done, here's how you did!</h2>
+  <p>Correct Answers: 3</p>
+  <p>Incorrect Answers: 4</p>
+  <p>Unanswered: 1</p>
+</div> 
+*/
+
+/*
+Component #8, 'START OVER' button
+<div class="cell">
+  <button id="js-reset-button" class="button large alert">Start Over?</button>
+</div>
+*/
