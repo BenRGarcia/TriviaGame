acb98a97661ca0257d8ab9141e08816721160380
Ben Garcia <ben@seebenprogram.com>
2018-02-11 13:44:34 -0500

    Add 'export' for modularity

--- assets/javascript/model.js
+++ assets/javascript/model.js
@@ -1,31 +1,34 @@
 // MODEL - directly manages the data, logic and rules of the application
 
 /*
- * Object "gifs"
+ * Object Directory:
+ * 
+ * "gifs"
  *     - Contains files paths to animated .gif's
  * 
- * Object "countDownTimer"
+ * "countDownTimer"
  *     - Accepts initial set time, counts down by second,
  *       publishes state change to subscribers
  * 
- * Object "triviaProps"
+ * "triviaProps"
  *     - Manages trivia game's state
  * 
- * Object "triviaQuestions"
+ * "triviaQuestions"
  *     - Holds bank of trivia questions, choices to offer, 
  *       and correct answer
- * 
  */
 
-// File paths to .gif's based on game state
-const gifs = {
+/*===========================================================================*/
+// Contains files paths to animated .gif's
+export const gifs = {
   correct:   "./assets/images/dancing-earth.gif",
   incorrect: "./assets/images/sad-earth.gif",
   gameOver:  "./assets/images/happy-earth.gif"
 };
 
-// countDownTimer object to count down time remaining, publish state to subscribers
-const countDownTimer = {
+/*===========================================================================*/
+// Accepts initial set time, counts down by second, publishes state change to subscribers
+export const countDownTimer = {
   _timeRemaining: 0,
   _intervalId: 0,
   _subscribers: [],
@@ -41,25 +44,17 @@ const countDownTimer = {
     }
   },
 
-  set timeRemaining(minutes, seconds) {
-    if (typeof minutes === "number" &&
-        typeof seconds === "number" &&
-        seconds < 60) 
+  set timeRemaining(seconds) {
+    if (typeof seconds === "number") 
     {
-      let totalSeconds = this.convertToSeconds(minutes, seconds);
-      this._timeRemaining = totalSeconds;
+      this._timeRemaining = seconds;
       this.publish();
-      return totalSeconds;
+      return seconds;
     } 
     else 
     {
       console.log(`countDownTimer object's 'timeRemaining' property not set. '${minutes}' and '${seconds}' are not both numbers.`);
     }
-  },
-
-  convertToSeconds(minutes, seconds) {
-    let totalSeconds = (minutes * 60) + seconds;
-    return totalSeconds;
   },
 
   start() {
@@ -69,7 +64,6 @@ const countDownTimer = {
     return this._intervalId;
   },
 
-  // Decrement time remaining only if time isn't up, otherwise reset countDownTimer
   countDown() {
     if (this._timeRemaining > 0) {
       this._timeRemaining--;
@@ -83,7 +77,6 @@ const countDownTimer = {
     return this._timeRemaining;
   },
 
-  // method that will push out countDownTimer state change to subscribers
   publish() {
     let totalSubscribers = this._subscribers.length;
     for (let i = 0; i < totalSubscribers; i++) {
@@ -97,7 +90,9 @@ const countDownTimer = {
   },
 };
 
-const triviaProps = {
+/*===========================================================================*/
+// Manages trivia game's state
+export const triviaProps = {
   _correctCount: 0,
   _incorrectCount: 0,
   _unansweredCount: 0,
@@ -144,25 +139,26 @@ const triviaProps = {
     console.log(`The player guessed ${guess}, and the answer is: ${this.answer}`);
     if (guess === this.answer) {
       console.log(`Correct!`);
-      // this.incrementCorrect();
       return true;
     } else {
       console.log(`Incorrect!`);
-      // this.incrementIncorrect();
       return false;
     }
   },
 
   incrementCorrect() {
     this._correctCount++;
+    return this._correctCount;
   },
 
   incrementIncorrect() {
     this._incorrectCount++;
+    return this._incorrectCount;
   },
 
   incrementUnanswered() {
     this._unansweredCount++;
+    return this._unansweredCount;
   },
 
   resetGame() {
@@ -170,11 +166,13 @@ const triviaProps = {
     this._incorrectCount = 0;
     this._unansweredCount = 0;
     this._questionObj = {};
+    return this;
   }
 };
 
-// Question Bank
-const triviaQuestions = {
+/*===========================================================================*/
+// Holds bank of trivia questions, choices to offer, and correct answer
+export const triviaQuestions = {
   _questionObj: [
     {
       question: "In which month is the Earth closest to the Sun?",
@@ -222,3 +220,5 @@ const triviaQuestions = {
     return this._questionObj[index];
   }
 };
+
+/*===========================================================================*/
