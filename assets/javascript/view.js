d7f9577a0dad7e4ae0c74a6cf22e43fe45ccacd7
Ben Garcia <ben@seebenprogram.com>
2018-02-11 13:43:24 -0500

    Initial commit

---
+++ assets/javascript/view.js
@@ -0,0 +1,28 @@
+import * as component from './components.js';
+
+// VIEW - output representation of information
+
+
+
+
+
+
+// rendering object to go here
+
+// Views that need to be assembled, rendered
+/*
+ * Game Begins:
+ *     - Component #1
+ * 
+ * Question Asked:
+ *     - Components #2, #3, #4
+ * 
+ * Question Answered wrong:
+ *     - Components #2, #3, #5
+ * 
+ * Question Answered right:
+ *     - Components #2, #3, #6
+ * 
+ * Game Over:
+ *     - Components #7, #8
+ */
