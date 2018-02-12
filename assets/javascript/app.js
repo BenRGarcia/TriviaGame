bddf84ade6a95248758d06642d7e8db9c6c61257
Ben Garcia <ben@seebenprogram.com>
2018-02-12 09:39:26 -0500

    Add copious amounts of pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,19 +24,146 @@
  * 
  */
 
-const triviaGame = {
-  questionsLeft: 0,
-  questionIsActive: false,
+ /* FUNCTION # 1 */
+ // clickHandler()
+
+  // split clicks into 2 categories: 1) start/start over, 2) player chose an answer
+
+    // 1) start/start over
+      // start the game by asking 1st question, call start()
+
+    // 2) player chose an answer
+      // pass the answer choice to a function that evaluates if right or wrong
+
+
+/* FUNCTION # 2 */
+// nextQuestion()
+
+  // if questions left is > 0 (still more questions to ask)
   
-  // Handle user clicks, 2 categories: a) start/start over, b) question guess
+    // set _questionIsActive to true
+    // decrement _questionsLeft
+    // render next question to appear on the page (index of question array at [length - 1])
+    // start timer countdown
+
+  // else game is over, call gameIsOver()
+
+
+/* FUNCTION # 3 */
+// evaluate(choice)
+
+  // Because answer choice was received, stop timer
+  // set _questionIsActive to false
+
+  // evaluate the user's guess if right or wrong
+
+    // If right, call triviaGame.guessWasRight()
+    // If wrong, call triviaGame.guessWasWrong()
+
+
+/* FUNCTION # 4 */
+// guessWasRight()
+
+  // increment triviaProps correctCount
+  // render appropriate gif/transitional view to page
+  // set timer for 5 seconds before triggering next question
+
+
+/* FUNCTION # 5 */
+// guessWasWrong()
+
+  // increment triviaProps incorrectCount
+  // render appropriate gif/transitional view to page
+  // set timer for 5 seconds before triggering next question
+
+
+/* FUNCTION # 6 */
+// guessWasUnanswered()
+
+  // set isQuestionActive to false
+  // increment triviaProps unansweredCount
+  // render appropriate gif/transitional view to page
+  // set timer for 5 seconds before triggering next question
+
+
+/* FUNCTION # 7 */
+// setTimer(seconds)
+
+  // will call on countDownTimer to set the initial timer amount, automatically countdown/broadcast
+
+
+/* FUNCTION # 8 */
+// stopTimer()
+
+  // will call on countDownTimer to stop/reset everything
+
+
+/* FUNCTION # 9 */
+// receiveTimerData(secondsRemaining)
+
+  // will receive automated broadcasts of seconds remaining
+  // will send seconds remaining to DOM.render()
+
+  // 1) Only take action if time left is 0
+
+    // Stop/reset timer since it's no longer needed now that time has run out
+
+    // 2) Determine if questionIsActive (whether or not player is on a question or a transition)
+
+      // 3a) If on a question, (player ran out of time)
+        // call guessWasUnanswered()
+
+      // 3a) If on a transition, time for the next question
+        // call nextQuestion()
+
+
+/* FUNCTION # 10 */
+// gameIsOver()
+
+  // call this function when there are no questions remaining
+  // render game over view to page
+
+
+/* FUNCTION # 11 */
+
+
+/* FUNCTION # 12 */
+
+
+
+
+/* FUNCTION # 13 */
+// start()
+
+  // reset triviaProps object
+  // find out how many questions there are, set _questionsLeft
+  // call triviaGame.nextQuestion()
+
+
+/* FUNCTION # 14 */
+// initialize()
+
+  // (only runs when the page is first loaded)
+  // add subscribers to subscriber list in countDownTimer
+  // render start button to page
+
+
+
+const triviaGame = {
+  _questionsLeft: 0,
+  _questionIsActive: false,
+  _questionTimeLimit: 10,
+  _transitionTimeLimit: 5,
+  
+  // Handle user clicks, 2 categories: a) start/start over, b) player chose an answer
   clickHandler(clickValue) {
 
     // Player is ready to start game
-    if (clickValue === "START" || clickValue === "START OVER?") {
+    if (clickValue === "START" || clickValue === "START OVER?")
+    {
       console.log(`Click value '${clickValue}' received by triviaGame.clickHandler`);
       // Start new round
       this.startGame();
-      this.nextQuestion();
     } 
     // User has guessed the answer to the trivia question
     else {
@@ -48,11 +175,11 @@ const triviaGame = {
   // Is user guess right or wrong?
   evalute(choice) {
 
-    // Clear countDownTimer b/c guess was received
+    // Clear countDownTimer b/c guess was received before time ran out
     countDownTimer.reset();
     
     console.log(`Player guess received by triviaGame.evaluate(), and...`);
-    if (choice === triviaProps.answer) {
+    if (triviaProps.isAnswerCorrect(choice)) {
       // user guess was right
       console.log(`User guess of ${choice} was correct!!`);
       this.guessWasRight();
@@ -63,34 +190,45 @@ const triviaGame = {
     }
   },
 
+  // For when the player guessed right
   guessWasRight() {
-    // Do stuff... when the player guessed right
-    this.questionIsActive = false;
+    this._questionIsActive = false;
+    this.setTimer(this._transitionTimeLimit);
     triviaProps.incrementCorrect();
+    console.log(`Because the player's guess was right, the correctly answered count is now: ${triviaProps.correctCount}`);
   },
 
+  // For when the player guessed wrong
   guessWasWrong() {
-    // Do stuff... when the player guessed wrong
-    this.questionIsActive = false;
+    this._questionIsActive = false;
+    this.setTimer(this._transitionTimeLimit);
     triviaProps.incrementIncorrect();
+    console.log(`Because the player's guess was wrong, the incorrectly answered count is now: ${triviaProps.incorrectCount}`);
   },
 
+  // For when the player did not guess in the alloted time
   questionWasUnanswered() {
-    this.questionIsActive = false;
+    this._questionIsActive = false;
+    this.setTimer(this._transitionTimeLimit);
+    triviaProps.incrementUnanswered();
+    console.log(`Because the player did not guess, the unanswered count is now: ${triviaProps.unansweredCount}`);
   },
   
+  // Add question and answer choices to the screen
   nextQuestion() {
+    console.log(`triviaGame.nextQuestion was just called!`);
 
-    this.questionIsActive = true
+    this._questionIsActive = true;
 
-    // start the timer countdown
-    countDownTimer.setTimer = 15;
     // Remove contents of #js-page-content before adding new content
     // DOM.render('clearScreen');
 
     // only set next question if more questions remain
-    if (this.questionsLeft > 0) {
+    if (this._questionsLeft > 0) {
       // load next question to question object
+      triviaProps.questionObj = triviaQuestions._questionObj[this._questionsLeft - 1]
+      this.setTimer(this._questionTimeLimit);
+      this._questionsLeft--;
 
     } else {
       // game is over, show game stats and 'START OVER?' button
@@ -100,42 +238,22 @@ const triviaGame = {
 
   // Set countDownTimer's initial count down amount
   setTimer(seconds) {
-    countDownTimer.timeRemaining = seconds;
+    countDownTimer.setTimeRemaining = seconds;
+    console.log(`triviaGame just called countDownTimer.timeRemaining to be ${seconds} seconds.`);
+    countDownTimer.start();
   },
 
   receiveTimerData(secondsRemaining) {
-
-    if (this.questionIsActive) {
-      // countdown is for question
-      if (secondsRemaining <= 0) {
-        console.log(`Time to answer the question ran out!`);
-        // question went unanswered when time ran out
-        countDownTimer.reset();
-        this.questionWasUnanswered();
-      }
-    } else {
-      // countdown is for transition to next question
-      if (secondsRemaining <= 0) {
-        // animated gif on screen already, time to queue next question
-      }
-    }
-
-    // If time runs out
-    if (secondsRemaining <= 0) {
-      countDownTimer.reset();
-      this.timeRanOut();
-    } else {/* Do nothing... */}
+    console.log(`triviaGame.receiveTimerData just received ${secondsRemaining}`);
   },
 
   startGame() {
     triviaProps.resetGame();
-    this.questionsLeft = triviaQuestions._questionObj.length;
+    this._questionsLeft = triviaQuestions._questionObj.length;
     this.nextQuestion();
   },
 
   initialize() {
-
-    this.startGame();
 
     // Add subscribers to countDownTimers
     countDownTimer.addSubscriber = triviaGame;
@@ -146,6 +264,26 @@ const triviaGame = {
 
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Approved code:
+
+
+
+
 
 // When game first starts, initialize round
 triviaGame.initialize();
